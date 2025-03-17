64874be50b0a59695335a09d12b9c56f617151b2
rizwanahmed1981 <razykan.ra@gmail.com>
2025-03-18 02:22:31 +0500

    streamlit web app added

---
+++ streamlit-app/main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("SImple Data DashBoard")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    # st.write("File Uploaded...")
+    df = pd.read_csv(uploaded_file)
+
+    st.subheader("Data Preveiw")
+    st.write(df.head())
+
+    st.subheader('Data Summary')
+    st.write(df.describe())
+
+    st.subheader("Filter Data")
+    columns = df.columns.tolist()
+    selected_columns = st.selectbox("Select Columns to Filter", columns)
+    unique_value = df[selected_columns].unique()
+    selected_value = st.selectbox("Select value", unique_value)
+
+    filtered_df = df[df[selected_columns] == selected_value]
+    st.write(filtered_df)
+
+    st.subheader("Plot Data")
+
+    x_column = st.selectbox("Select X-axis column", columns)
+    y_column = st.selectbox("Select Y-axis column", columns)
+
+    if st.button("Generate Plot"):
+        st.line_chart(filtered_df.set_index(x_column)[y_column])
+
+else:
+    st.write("Waiting file to be uploaded")
